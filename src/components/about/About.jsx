4ae0fb32ec0d3b2565ecd0cd28d7a87718535c6a
megamobile <olinor1211@gmail.com>
2022-07-12 01:50:00 +0800

    About Section Done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,49 @@
 import React from "react";
-import './about.css'
+import "./about.css";
+import ME from "../../assets/me-about.jpg";
+import { FaAward, FaUserFriends } from "react-icons/fa";
+import { VscNewFolder } from "react-icons/vsc";
+
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me_img">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__card_icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FaUserFriends className="about__card_icon" />
+              <h5>Clients</h5>
+              <small>20+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscNewFolder className="about__card_icon" />
+              <h5>Projects</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+            odio. Animi doloremque possimus saepe ipsum.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
