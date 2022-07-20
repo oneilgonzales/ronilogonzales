4d75ecf155d988bbf947b24d04e1a62edb7400cc
megamobile <olinor1211@gmail.com>
2022-07-21 04:23:45 +0800

    Services and Portfolio Done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,115 @@
 import React from "react";
 import "./services.css";
+import { FiCheck } from "react-icons/fi";
 const Services = () => {
-  return <section id="services">Services</section>;
+    return (
+        <section id="services">
+            <h5>What Services I Offer</h5>
+            <h2>Services</h2>
+            <div className="container services__container">
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Android App Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF Android Development */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Web Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+                {/* END OF Web Development */}
+                <article className="service">
+                    <div className="service__head">
+                        <h3>Chatbot Development</h3>
+                    </div>
+                    <ul className="service__list">
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                        <li>
+                            <FiCheck className="service__list-icon" />
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing.
+                            </p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+    );
 };
 
 export default Services;
