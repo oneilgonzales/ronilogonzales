b34b6074b216a4e083190691e32c5618b8fcadda
megamobile <olinor1211@gmail.com>
2022-08-04 00:35:49 +0800

    All Sections DONE

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
-import './footer.css'
-const Footer = () => {
-  return (
-    <section id='footer'>
-      Footer
-    </section>
-  )
-}
+import React from "react";
+import "./footer.css";
+import { FaFacebookF, FaTwitter } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
 
-export default Footer
+const Footer = () => {
+    return (
+        <footer>
+            <a href="#" className="footer__logo">
+                ONEIL GONZALES
+            </a>
+
+            <ul className="permalink">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#experience">Experience</a>
+                </li>
+                <li>
+                    <a href="#services">Services</a>
+                </li>
+                <li>
+                    <a href="#portfolio">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#testimonials">Testimonials</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://www.facebook.com/oneil.gonzales.1">
+                    <FaFacebookF />
+                </a>
+                <a href="https://www.instagram.com/olinor1211/">
+                    <BsInstagram />
+                </a>
+                <a href="https://twitter.com/olinor1211">
+                    <FaTwitter />
+                </a>
+            </div>
+            <div className="footer__copyright">
+                <small>&copy; All rights reserved</small>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
